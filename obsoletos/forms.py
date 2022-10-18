4c88e1ef7b541a9c8addebeea049ea120d1f747f
Xerchio <xerchion@gmail.com>
2022-10-18 07:13:18 +0200

    18-10 heroku 5

--- obsoletos/forms.py
+++
@@ -1,8 +0,0 @@
-from tkinter.tix import Select
-from wtforms import Form
-from wtforms import SelectField,StringField
-
-
-class Datos(Form):
-    year=StringField("Año")
-    turno=StringField("Turno")
--- obsoletos/htmlcalendar.py
+++
@@ -1,6 +0,0 @@
-import calendar
-
-
-calendario_HTML = calendar.HTMLCalendar(calendar.MONDAY)
-tablaHTML = calendario_HTML.formatmonth(2020, 2)
-print(tablaHTML)
